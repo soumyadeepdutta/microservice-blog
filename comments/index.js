aa99adcfd28d6fe65b18629e6c3a96153df3aa92
soumyadeepdutta <imsoumyadeepdutta@gmail.com>
2022-01-02 00:48:32 +0530

    created clusterip service for all microservices
    all microservices are communicating through the clusterip service of kubernetees

--- comments/index.js
+++ comments/index.js
@@ -25,7 +25,7 @@ app.post("/posts/:id/comments", async (req, res) => {
     })
     comments[req.params.id] = comment
 
-    await axios.post("http://localhost:4005/events", {
+    await axios.post("http://event-bus-srv:4005/events", {
         type: 'CommentCreated',
         data: {
             id,
@@ -49,7 +49,7 @@ app.post("/events", async (req, res) => {
         })
         modifiedComment.status = status
 
-        await axios.post('http://localhost:4005/events', {
+        await axios.post('http://event-bus-srv:4005/events', {
             type: 'CommentUpdated',
             data: {
                 id,
--- posts/index.js
+++ posts/index.js
@@ -21,7 +21,8 @@ app.post("/posts", async (req, res) => {
         id, title
     }
     // raise an event
-    await axios.post('http://localhost:4005/events', {
+    // using clusterip service to send data
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'PostCreated',
         data: {
             id, title
